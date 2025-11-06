ca237043c2629d50c97cb9c03eed585320bf0770
aniketmaster09-alt <aniketmaster09@gmail.com>
2025-11-06 08:18:09 +0530

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -1,13 +1,11 @@
 const ytdl = require('ytdl-core');
-const { HttpsProxyAgent } = require('https-proxy-agent');
-
-const proxyAgent = new HttpsProxyAgent('http://127.0.0.1:8080');
 
 const options = {
   requestOptions: {
-    agent: proxyAgent,
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
     }
   }
 };
