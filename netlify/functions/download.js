3c455704895de3bacb52fa3c7adcf7116d16cec9
aniketmaster09-alt <aniketmaster09@gmail.com>
2025-11-06 08:20:50 +0530

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -3,9 +3,17 @@ const ytdl = require('ytdl-core');
 const options = {
   requestOptions: {
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       'Accept-Language': 'en-US,en;q=0.9',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'DNT': '1',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'Sec-Fetch-Dest': 'document',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Site': 'none',
+      'Cookie': 'VISITOR_INFO1_LIVE=; YSC=; PREF=f1=50000000&f6=40000000&hl=en&gl=US'
     }
   }
 };
