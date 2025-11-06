9a2aa3a6d4e4e6c106fed4c2c7366ed8102b802f
aniketmaster09-alt <aniketmaster09@gmail.com>
2025-11-06 08:25:14 +0530

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -6,14 +6,7 @@ const options = {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       'Accept-Language': 'en-US,en;q=0.9',
       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'DNT': '1',
-      'Connection': 'keep-alive',
-      'Upgrade-Insecure-Requests': '1',
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'none',
-      'Cookie': 'VISITOR_INFO1_LIVE=; YSC=; PREF=f1=50000000&f6=40000000&hl=en&gl=US'
+      'Cookie': 'HSID=AA3F5nt2q6TBlB6mT; SSID=AhfB7LFmPrgT_9ngl; APISID=gxR1ISU2RT3y7mbW/A62PCHOlM9r3yg_sD; SAPISID=Lr-zkJdlb0GdVHX1/AWgsjyeanFJQc6Abz; __Secure-1PAPISID=Lr-zkJdlb0GdVHX1/AWgsjyeanFJQc6Abz; __Secure-3PAPISID=Lr-zkJdlb0GdVHX1/AWgsjyeanFJQc6Abz; SID=g.a0003AjjJc9pmisaHyHLSlmbpjbfUfELmkuPqVb_Y483jrcXetpAVp-eT60_XI2BVfdTC2aHhQACgYKAdsSARASFQHGX2Mi6CsG3rLL77r5Kbta_qF2lxoVAUF8yKq-O2HBmxnO3N_nR_MTkz860076; __Secure-1PSID=g.a0003AjjJc9pmisaHyHLSlmbpjbfUfELmkuPqVb_Y483jrcXetpAm5S2BSWTctI0Ms0-kPZW8gACgYKAfoSARASFQHGX2MiLpLX6DxPLIM7AwgQYwC82hoVAUF8yKoS0Tscfbo7AY6MZnVmo1TB0076; __Secure-3PSID=g.a0003AjjJc9pmisaHyHLSlmbpjbfUfELmkuPqVb_Y483jrcXetpA-KcPZD9nNJC0qp4ttPQ1twACgYKAZkSARASFQHGX2Mi8Fg4USrkKsaU_OS8idz8GBoVAUF8yKr1GI_7HP3kgrDHxu852p1M0076; VISITOR_INFO1_LIVE=0RJBxR1apDs; YSC=SX4urkp40ho; PREF=f4=4000000&f6=40000000&tz=Asia.Calcutta&f7=100'
     }
   }
 };
